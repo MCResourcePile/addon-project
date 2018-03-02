e4b816d9c86a6c6ea86eebdede4d3df893ff9b17
Mitchell <mitchiiii@users.noreply.github.com>
2018-03-02 11:44:34 +1100

    Remove console logging

--- src/templates/helpers/keyPath.js
+++ src/templates/helpers/keyPath.js
@@ -5,12 +5,9 @@ module.exports = function keyPath(obj, value, path) {
     }
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) {
-          console.log(value);
-          console.log(path);
         var t = path;
         var v = obj[key];
         newPath = path + '/' + key;
-          console.log(newPath);
         if (v === value) {
           return newPath;
         } else if (typeof v !== 'object') {
